5b6ae147cdb002f0df3360fe7ae5676d8baab430
Ruke-dev <tugara2017@gmail.com>
2026-06-09 23:25:28 +0530

    fixed the backspace bug

--- main.js
+++ main.js
@@ -66,8 +66,10 @@ function handleSymbol(value){
         case '←':
             if(buffer.length === 1){
                 buffer = "0";
+                break;
             }else{
                 buffer = buffer.substring(0, buffer.length - 1);
+                break;
             }
         case '+':   
         case '-':
